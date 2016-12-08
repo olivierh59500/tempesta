0ef9c2a06eb20c1a38e486089c7ea5043b73a17d
Sergey Lvov <sl@natsys-lab.com>
2016-12-08 11:44:54 +0300

    init

--- tempesta_fw/t/functional/tests/__init__.py
+++ tempesta_fw/t/functional/tests/__init__.py
@@ -3,4 +3,4 @@ __copyright__ = 'Copyright (C) 2016 Tempesta Technologies Inc. (info@natsys-lab.
 __license__ = 'GPL2'
 
 __all__ = [ "bomber", "fragmented_requests", "test_frang", "test_cache",
-	    "test_parser" ]
+	    "test_parser", "test_sched" ]
